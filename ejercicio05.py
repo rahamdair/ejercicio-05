5ccf2b959bb80348f13f680561c381e57e74bdad
Raham Castillo <raham_27@hotmail.com>
2024-05-08 21:04:41 -0500

    Ejercicio-05

--- ejercicio05.py
+++ ejercicio05.py
@@ -4,6 +4,5 @@ with open("frases.csv", "r", encoding="utf8") as archivo:
     frases = archivo.readlines()
 
 for linea in frases:
-    #print(linea)
     r = requests.get("http://127.0.0.1:8080/prediccion/'%s'" % linea)
     print(f'La frase: {linea} es: {r.json()['prediction']}')
